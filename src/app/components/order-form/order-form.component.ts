55d8de40bd656e76a8655b28de46978c22e82926
Alexander Govorkov <agcentory240@gmail.com>
2024-08-25 14:30:58 +0300

    orders

--- src/app/components/order-form/order-form.component.ts
+++ src/app/components/order-form/order-form.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { AlertController, ModalController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, ToastController } from '@ionic/angular';
 import { UserService } from '../../services/user.service';
 import { Clipboard } from '@capacitor/clipboard';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { finalize } from 'rxjs';
 const IMAGE_DIR = 'stored-images';
 
 export interface OrderFormItem {
@@ -16,6 +19,7 @@ export interface OrderFormItem {
   images_count: number;
   images: any[];
   images_b64: any[];
+  images_names: string[];
 }
 
 @Component({
@@ -31,12 +35,14 @@ export class OrderFormComponent implements OnInit {
     attrubutes: "",
     qty: 1,
     price: "",
+    additional: "",
     images: [],
     images_b64: [],
+    images_names: [],
     images_count: 0
   }]
 
-  constructor(private alertController: AlertController, public sanitizer: DomSanitizer, private userService: UserService, private modalCtrl: ModalController) { }
+  constructor( private toastController: ToastController, private http: HttpClient, private loadingCtrl: LoadingController, private alertController: AlertController, public sanitizer: DomSanitizer, private userService: UserService, private modalCtrl: ModalController) { }
 
   ngOnInit() { }
 
@@ -90,12 +96,15 @@ export class OrderFormComponent implements OnInit {
       price: "",
       images: [],
       images_b64: [],
+      images_names: [],
       images_count: 0
     })
   }
 
 
   async addImage(card: OrderFormItem) {
+
+    console.log("card",card.images);
 
     //Если директрия для картинок не создана, то создадатим
     Filesystem.readdir({
@@ -136,10 +145,11 @@ export class OrderFormComponent implements OnInit {
       const formData = new FormData();
       const response = await fetch(image.webPath!);
       const blob = await response.blob();
-      card.images.push(blob);
       this.convertBlobToBase64(blob).then(res => {
         card.images.push(blob);
         card.images_b64.push(res);
+        card.images_names.push(image.path!);
+        console.log("add image to array",card.images_names);
       })
     }
   }
@@ -188,11 +198,87 @@ export class OrderFormComponent implements OnInit {
     if (role == 'confirm') {
       card.images.splice(i, 1);
       card.images_b64.splice(i, 1);
-
+      card.images_names.splice(i, 1);
+      console.log("delete image from array",card.images_names);
 
     }
   }
 
 
+
+  valid(): boolean {
+    for (const item of this.cards) {
+      // Проверяем, что поле url не является пустой строкой, qty не равен null или undefined, и price не является пустой строкой
+      if (!item.url.trim() || item.qty == null || item.price.trim() === '') {
+        return false; // Если хотя бы одно из полей не заполнено, возвращаем false
+      }
+    }
+    return true; // Если все элементы массива прошли проверку, возвращаем true
+  }
+
+
+
+  async send() {
+
+    console.log("cards",this.cards);
+
+    const loading = await this.loadingCtrl.create({
+      message: 'Отправляем данные...',
+      mode: "ios",
+    });
+    await loading.present(); 
+
+    let user_id = localStorage.getItem(environment.prefix + 'user_id') || "";
+    const formData = new FormData();
+    formData.append('user_id', user_id);
+    formData.append('count', this.cards.length.toString());
+
+    this.cards.forEach( (card:OrderFormItem, i:number) => {
+      formData.append('url_'+i.toString(), card.url);
+      formData.append('attrubutes_'+i.toString(), card.attrubutes);
+      formData.append('additional_'+i.toString(), card.additional || "");
+      formData.append('price_'+i.toString(), card.price);
+      formData.append('qty_'+i.toString(), card.qty.toString());
+      card.images.forEach((image:Blob, image_index:number) => {
+        console.log("formData.append",image_index,card.images_names[image_index]);
+        formData.append('image_'+i.toString()+"_"+image_index.toString(), image, card.images_names[image_index].replace(/^.*[\\/]/, ''));
+      });
+      formData.append('images_count_'+i.toString(), card.images.length.toString());
+    });
+
+    const url = environment.api_url + 'upload_order.php';  
+    this.http.post(url, formData)
+    .pipe(
+        finalize(() => {
+            loading.dismiss();
+        })
+    )
+    .subscribe(async (res:any) => {
+      console.log("upload res",res);
+      if (res.success) {
+        const toast = await this.toastController.create({
+          message: res.message,
+          duration: 4000,
+          color: "accent",
+          icon:"checkmark-outline",
+          position: "bottom"
+        });
+        toast.present();
+        this.modalCtrl.dismiss();
+      } else {
+        const toast = await this.toastController.create({
+          message: res.message,
+          duration: 4000,
+          color: "danger",
+          position: "bottom",
+          icon:"alert-circle-outline",
+        });
+        toast.present();
+      }
+    }, (error:any)=>{
+      console.log("upload error",error);
+    });
+    // loading.dismiss();
+  }
 
 }
